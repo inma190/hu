4be6368c526c3db8e121edce84a110e070b2604d
inma190 <134674173+inma190@users.noreply.github.com>
2023-05-26 07:04:41 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,46 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Start")
+})
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    # . # . #
+    # . # . #
+    `)
+basic.showLeds(`
+    . # # # .
+    . # # # .
+    # # # # #
+    # # . # #
+    . # . # .
+    `)
+basic.showLeds(`
+    . # # # .
+    . # # # .
+    # # # # #
+    # # # # #
+    . # . # .
+    `)
+basic.showLeds(`
+    # . # . .
+    # # # . .
+    . . # . .
+    # # # . .
+    # . # . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.showLeds(`
+    . # . # .
+    . # . # .
+    # # # # #
+    . # . # .
+    # # . # #
+    `)
+basic.showIcon(IconNames.Heart)
